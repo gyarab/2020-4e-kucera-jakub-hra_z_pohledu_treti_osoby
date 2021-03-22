737d041a2621098c65395f0e669f00e33076664b
Jakub <jakub.kucera@student.gyarab.cz>
2021-03-22 19:57:32 +0100

    Build

--- Assets/Editor/ScriptableObjectIDManager.cs
+++ Assets/Editor/ScriptableObjectIDManager.cs
@@ -43,7 +43,7 @@ public class ScriptableObjectIDManager : EditorWindow
         AssetDatabase.SaveAssets();
     }
 
-    // Najde všechny objekty daného typu v projektu
+    // Najde všechny objekty daného typu v projektu; převzato od glitchers
     private List<T> FindAssetsByType<T>() where T : UnityEngine.Object
     {
         List<T> assets = new List<T>();
